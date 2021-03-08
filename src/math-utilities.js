f5eaacf1576585367fe2e75ef331321d4569e574
Mathias Isaksen <mathiasleanderi@gmail.com>
2021-03-08 20:36:00 +0100

    Implements Path function and computePathEdgeIntersection

--- src/math-utilities.js
+++ src/math-utilities.js
@@ -57,13 +57,19 @@ function subtractVectors(vector1, vector2) {
     return(Vector(differenceX, differenceY));
 }
 
+function scaleVector(vector, scalar) {
+    const scaledX = scalar*vector.getX();
+    const scaledY = scalar*vector.getY();
+    return(Vector(scaledX, scaledY));
+}
+
 function createUnitVector(direction) {
     return(Vector(Math.cos(direction), Math.sin(direction)));
 }
 
 function Edge(startVertex, endVertex) {
-    let _startVertex = Vector(startVertex.x, startVertex.y);
-    let _endVertex = Vector(endVertex.x, endVertex.y);
+    let _startVertex = startVertex;
+    let _endVertex = endVertex;
     let _diffVector = subtractVectors(_endVertex, _startVertex);
 
     function getLength() {
@@ -78,7 +84,52 @@ function Edge(startVertex, endVertex) {
         return(_endVertex);
     }
 
-    return({ getLength, getStartVertex, getEndVertex });
+    function getDifferenceVector() {
+        return(_diffVector);
+    }
+
+    return({ getLength, getStartVertex, getEndVertex, getDifferenceVector });
 }
 
-export { Vector, crossProduct2D, addVectors, subtractVectors, createUnitVector, Edge };
+function Path(initialPoint, directionVector) {
+    let _initialPoint = initialPoint;
+    let _directionVector = directionVector;
+
+    function getInitialPoint() {
+        return(_initialPoint);
+    }
+
+    function getDirectionVector() {
+        return(_directionVector);
+    }
+
+    function getPositionAtTime(time) {
+        return(addVectors(_initialPoint, scaleVector(_directionVector, time)));
+    }
+
+    return({ getInitialPoint, getDirectionVector, getPositionAtTime });
+}
+
+// Useful reference: Intersection of two line segments
+function computePathEdgeIntersection(path, edge) {
+    // Path is on the form pathStart + pathVector*t, where t >= 0
+    const pathStart = path.getInitialPoint();
+    const pathVector = path.getDirectionVector();
+
+    // Edge is on form edgeStart + edgeVector*u, where 0 <= s <= 1
+    const edgeStart = edge.getStartVertex();
+    const edgeVector = edge.getDifferenceVector();
+
+    // Intersection when t = (edgeStart - pathStart) × edgeVector / (pathVector × edgeVector)
+    //                or u = (edgeStart - pathStart) × pathVector / (pathVector × edgeVector)
+    const startDiff = subtractVectors(edgeStart, pathStart);
+    const denominator = crossProduct2D(pathVector, edgeVector);
+    const t = crossProduct2D(startDiff, edgeVector) / denominator;
+    const u = crossProduct2D(startDiff, pathVector) / denominator;
+    const intersectionPoint = path.getPositionAtTime(t);
+    return({ intersectionPoint, pathParameter: t, edgeParameter: u});
+}
+
+export { Vector, crossProduct2D, addVectors, 
+    subtractVectors, createUnitVector, Edge, Path,
+    computePathEdgeIntersection };
