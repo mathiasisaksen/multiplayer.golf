f33f05d470f274f2ec0edc13f6800cca1a2cd89c
mathiasisaksen <mathiasleanderi@gmail.com>
2021-03-31 14:31:32 +0200

    Implements switching between player list and chat, starts courses element

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,7 +1,37 @@
 
-const toggleSidebarButton = document.querySelector('#sidebar-toggle-large-screen');
-const filler = document.querySelector('#sidebar-body-large-screen');
+const Sidebar = (() => {
+    const chatPlayersEnum = {PLAYERS: 'players', CHAT: 'chat'}
+    let currentChatOrPlayers = chatPlayersEnum.PLAYERS;
 
-toggleSidebarButton.addEventListener('click', () => filler.classList.toggle('hidden'));
+    const toggleSidebarButton = document.querySelector('#sidebar-toggle-large-screen');
+    const sidebarBody = document.querySelector('#sidebar-body-large-screen');
 
-export {};
+    const playerButton = sidebarBody.querySelector('#show-players-large-screen');
+    playerButton.addEventListener('click', handlePlayerClick);
+    const chatButton = sidebarBody.querySelector('#show-chat-large-screen');
+    chatButton.addEventListener('click', handleChatClick);
+    const chatPlayerTitle = sidebarBody.querySelector('#chat-player-title-large-screen');
+
+    function handlePlayerClick() {
+        if (currentChatOrPlayers === chatPlayersEnum.PLAYERS) return;
+        currentChatOrPlayers = chatPlayersEnum.PLAYERS;
+
+        chatButton.classList.remove('chat-player-selected');
+        playerButton.classList.add('chat-player-selected');
+        chatPlayerTitle.textContent = currentChatOrPlayers;
+    }
+
+    function handleChatClick() {
+        if (currentChatOrPlayers === chatPlayersEnum.CHAT) return;
+        currentChatOrPlayers = chatPlayersEnum.CHAT;
+        
+        playerButton.classList.remove('chat-player-selected');
+        chatButton.classList.add('chat-player-selected');
+        chatPlayerTitle.textContent = currentChatOrPlayers;
+    }
+
+    toggleSidebarButton.addEventListener('click', () => sidebarBody.classList.toggle('hidden'));
+
+})();
+
+export default Sidebar;
