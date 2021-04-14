975e667d098970dd227a345917a993d53a048337
mathiasisaksen <mathiasleanderi@gmail.com>
2021-04-14 22:28:52 +0200

    Adds announcement for player leaving game

--- src/online-game-handler/handle-player-left.js
+++ src/online-game-handler/handle-player-left.js
@@ -1,5 +1,6 @@
 import ChatBox from "../sidebar/chat-box";
 import PlayerList from "../sidebar/player-list";
+import showAnnouncement from "../sidebar/show-announcement";
 import OnlineGameHandler from "./online-game-handler";
 
 function handlePlayerLeft(data) {
@@ -10,10 +11,16 @@ function handlePlayerLeft(data) {
     if (data.currentPlayerLeft) {
         onlineGame.resetGolfBall();
         if (data.isNewCourse) {
-            onlineGame.setGameContent(data.newCourseData);
-            onlineGame.setCurrentCourseName(data.newCourseName);
+            showAnnouncement(`NEW HOLE, NEXT PLAYER: ${data.newCurrentPlayerName}`,() => {
+                onlineGame.setGameContent(data.newCourseData);
+                onlineGame.setCurrentCourseName(data.newCourseName);
+                onlineGame.setCurrentPlayer(data.newCurrentPlayerName);
+            });
+        } else {
+            showAnnouncement(`NEXT PLAYER: ${data.newCurrentPlayerName}`,() => {
+                onlineGame.setCurrentPlayer(data.newCurrentPlayerName);
+            });
         }
-        onlineGame.setCurrentPlayer(data.newCurrentPlayerName);
     } 
 }
 
